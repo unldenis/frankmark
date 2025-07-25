f812671c41c57017f76fbb5a07d0fcef99d59514
unldenis <mehillideny13@gmail.com>
2025-07-25 23:54:16 +0200

    templates: Responsive nav items & navigation buttons

--- src/config.rs
+++ src/config.rs
@@ -19,6 +19,18 @@ pub struct Book {
     pub github_url: Option<String>,
 }
 
+impl Book {
+    pub fn get_title_header(&self) -> String {
+        // max lenght 8 characters
+        // if more than 8 characters, add '...'
+        if self.title.len() > 8 {
+            self.title.split_at(8).0.to_string() + "..."
+        } else {
+            self.title.clone()
+        }
+    }
+}
+
 pub fn parse_config(config_path: &str) -> FrankmarkResult<Config> {
     let config_content = fs::read_to_string(config_path)?;
     let config: Config = toml::from_str(&config_content)?;
